6f22ec784914afb97925a155eddbbc835fe76342
jose.pereda <jose.pereda@gluonhq.com>
2022-09-29 11:14:30 +0200

    fix bat

--- .github/workflows/sccache-windows.bat
+++ .github/workflows/sccache-windows.bat
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
-
-set -e
 # Download and install sccache in the Windows specific cargo directories
-mkdir -p C:\Users\runneradmin\.cargo\bin
 curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.3.0/sccache-v0.3.0-x86_64-pc-windows-msvc.tar.gz | tar --strip-components=1 -C C:\Users\runneradmin\.cargo\bin -xzf -
+dir C:\Users\runneradmin\.cargo\bin
