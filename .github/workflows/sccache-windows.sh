2a09ce5ab44615bfc526f74c59a60e8156372ef8
jose.pereda <jose.pereda@gluonhq.com>
2022-09-29 10:46:02 +0200

    update paths

--- .github/workflows/sccache-windows.sh
+++ .github/workflows/sccache-windows.sh
@@ -2,6 +2,5 @@
 
 set -e
 # Download and install sccache in the Windows specific cargo directories
-mkdir -p /Users/runner/.cargo/bin
-curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-pc-windows-msvc.tar.gz | tar --strip-components=1 -C /Users/runner/.cargo/bin -xzf -
-chmod 755 /Users/runner/.cargo/bin/sccache
+mkdir -p C:\Users\runneradmin\.cargo\bin
+curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.3.0/sccache-v0.3.0-x86_64-pc-windows-msvc.tar.gz | tar --strip-components=1 -C C:\Users\runneradmin\.cargo\bin -xzf -
